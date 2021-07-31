003303d8768810b3a1b10ec2ffeb99ca5e668b07
Reski Mulud Muchamad <reski.mulud@gmail.com>
2021-07-31 18:13:03 +0700

    intro to JSX

--- src/App.js
+++ src/App.js
@@ -2,22 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const firstName = 'Reski Mulud';
+  const lastName = 'Muchamad';
+  const age = 20;
+  const job = 'Mahasiswa';
+
+  const getFullName = (firstName, lastName) => `${firstName} ${lastName}`;
+  const inputBox = <input placeholder="Enter Data" autoComplete />;
+
+  const myArr = [1, 2, 3, 4, 5];
+  const myObj = {
+    name: 'Reski Mulud Muchamad',
+    age: 20,
+    job: 'Mahasiswa'
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Nama Saya Adalah {getFullName(firstName, lastName)}</h3>
+      <p>Usia : {age} tahun</p>
+      <p>Pekerjaan : {job}</p>
+
+      {inputBox}
+
+      {myArr[0]}
+
+      <p>Nama Saya adalah {myObj.name}</p>
+
     </div>
   );
 }
